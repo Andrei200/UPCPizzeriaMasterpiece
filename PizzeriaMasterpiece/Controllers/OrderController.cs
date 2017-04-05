828a8272e3ee480a0cafedcc77592a9d0b393a51
KaloTester <kalo.tester@gmail.com>
2017-04-05 16:29:29 -0500

    Se agrego 1) diseño de pedido 2) parte del login y registro

---
+++ PizzeriaMasterpiece/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzeriaMasterpiece.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ActionResult MyOrder()
+        {
+            ViewBag.Message = "Your pizza page.";
+
+            return View();
+        }
+    }
+}
