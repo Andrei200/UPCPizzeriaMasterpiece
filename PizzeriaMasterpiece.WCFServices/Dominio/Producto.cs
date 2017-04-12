466d92faaa39bed9a876f121aa0472462088bb68
Tsubasa <proliverpr@hotmail.com>
2017-04-12 07:53:37 -0500

    Oliver

---
+++ PizzeriaMasterpiece.WCFServices/Dominio/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PizzeriaMasterpiece.WCFServices.Dominio
+{
+    [DataContract]
+    public class Producto
+    {
+        [DataMember]
+        public int IdProducto { get; set; }
+        [DataMember]
+        public int TipoProducto_IdTipoProducto { get; set; }
+        [DataMember]
+        public string NombreProducto { get; set; }
+        [DataMember]
+        public int Activo { get; set; }
+        [DataMember]
+        public double Precio { get; set; }
+        [DataMember]
+        public string Descripcion { get; set; }
+    }
+}
