c926e943fe627fbb02443404241e6fdb5ebb618c
SANDRO <sgamio13@hotmail.com>
2017-04-22 10:58:28 -0500

    order repo

--- PizzeriaMasterpiece.Repository/OrderRepository.cs
+++ PizzeriaMasterpiece.Repository/OrderRepository.cs
@@ -2,6 +2,7 @@
 using PizzeriaMasterpiece.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +12,35 @@ namespace PizzeriaMasterpiece.Repository
     public class OrderRepository
     {
 
-        //public async Task<OrderDTO> GetOrdersByClient(int clientId)
-        //{
-        //    using (var context = new PizzeriaMasterpieceEntities())
-        //    {
-        //        var result = await context.Orders.Where(p => p.UserId == clientId)
-        //            .Select(q => new ProductDTO
-        //            {
-        //                //ProductId = q.ProductId,
-        //                //Description = q.Description,
-        //                //Price = q.Price,
-        //                //Code = q.Code,
-        //                //Name = q.Name,
-        //                //ImagePath = q.ImagePath,
-        //                //SizeId = q.SizeId,
-        //                //SizeName = q.Size.Name,
-        //                //IsActive = q.IsActive,
-        //            }).FirstOrDefaultAsync();
+        public async Task<List<OrderDTO>> GetOrdersByClient(int userId)
+        {
+            using (var context = new PizzeriaMasterpieceEntities())
+            {
+                var result = await context.Orders.Where(p => p.UserId == userId)
+                    .Select(q => new OrderDTO 
+                    {
+                       OrderId  = q.OrderId ,
+                       OrderNo = q.OrderNo,
+                       Address = q.Address,
+                       Date = q.Date,
+                       Remark = q.Remark,
+                       DocumentTypeId = q.DocumentTypeId,
+                       DocumentTypeName = q.DocumentType.Name,
+                       OrderStatusId = q.OrderStatusId,
+                       OrderStatusName = q.OrderStatu.Name,
+                       OrderDetails = q.OrderDetails.Select(r => new OrderDetailDTO{
+                           OrderDetailId = r.OrderDetailId,
+                           ProductId = r.ProductId,
+                           ProductName = r.Product.Name,
+                           Price = r.Price,
+                           Quantity = r.Quantity,
+                           TotalPrice = r.TotalPrice
+                       }).ToList()
+                    }).ToListAsync();
 
-        //        return result;
-        //    }
-        //}
+                return result;
+            }
+        }
 
 
     }
---
+++ PizzeriaMasterpiece.WCFServices/OrderService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using PizzeriaMasterpiece.DTO;
+using System.Threading.Tasks;
+using PizzeriaMasterpiece.Repository;
+
+namespace PizzeriaMasterpiece.WCFServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "OrderService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select OrderService.svc or OrderService.svc.cs at the Solution Explorer and start debugging.
+    public class OrderService : IOrderService
+    {
+        public async Task<List<OrderDTO>> GetOrdersByClient(int userId)
+        {
+            var orderRepository = new OrderRepository();
+            return await orderRepository.GetOrdersByClient(userId);
+        }
+    }
+}
