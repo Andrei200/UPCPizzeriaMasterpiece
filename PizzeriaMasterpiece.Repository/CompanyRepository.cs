309de2dd8009f2689a2531cc3d1157cdac629038
Luis <liman.upc@hotmail.com>
2017-04-23 17:09:05 -0500

    Creacion de InsretCompany, Lista y UpdateCompany

--- PizzeriaMasterpiece.Repository/CompanyRepository.cs
+++ PizzeriaMasterpiece.Repository/CompanyRepository.cs
@@ -11,8 +11,6 @@ namespace PizzeriaMasterpiece.Repository
 {
   public  class CompanyRepository
     {
-        // crear  listado  de  todas  las companys sin parametro de  entrada 
-
         public async Task<CompanyDTO> GetCompany(int companyId)
         {
             using (var context = new PizzeriaMasterpieceEntities())
@@ -28,6 +26,59 @@ namespace PizzeriaMasterpiece.Repository
                     }).FirstOrDefaultAsync();
 
                 return result;
+            }
+        }
+
+        public async Task<CompanyDTO> InsertCompany(CompanyDTO company)
+        {
+            using (var context = new PizzeriaMasterpieceEntities())
+            {
+                var newCompany = new Company
+                {
+                    CompanyId = -1,
+                    Name = company.Name,
+                    RUC = company.RUC,
+                    Address = company.Address,
+                    PhoneNumber = company.PhoneNumber,
+                };
+
+                context.Companies.Add(newCompany);
+                context.SaveChanges();
+
+                return await GetCompany(newCompany.CompanyId);
+            }
+        }
+
+        public async Task<List<CompanyDTO>> GetCompanyList()
+        {
+            using (var context = new PizzeriaMasterpieceEntities())
+            {
+                var result = await context.Companies
+                .Select(q => new CompanyDTO
+                {
+                    CompanyId = q.CompanyId,
+                    Name = q.Name,
+                    RUC = q.RUC,
+                    Address = q.Address,
+                    PhoneNumber = q.PhoneNumber
+                })
+                .ToListAsync();
+
+                return result;
+            }
+        }
+
+        public async Task<CompanyDTO> UpdateCompany(CompanyDTO company)
+        {
+            using (var context = new PizzeriaMasterpieceEntities())
+            {
+                var currentCompany = await context.Companies.FindAsync(company.CompanyId);
+                if (!string.IsNullOrWhiteSpace(company.Name)) currentCompany.Name = company.Name;
+                if (!string.IsNullOrWhiteSpace(company.RUC)) currentCompany.Name = company.RUC;
+                if (!string.IsNullOrWhiteSpace(company.Address)) currentCompany.Name = company.Address;
+                if (!string.IsNullOrWhiteSpace(company.PhoneNumber)) currentCompany.Name = company.PhoneNumber;
+                context.SaveChanges();
+                return await GetCompany(currentCompany.CompanyId);
             }
         }
     }
