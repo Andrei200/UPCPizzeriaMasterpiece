2d8ff6aae128a1c70a4bdbc68219031b6ffa4a2b
Luis <liman.upc@hotmail.com>
2017-04-23 16:29:16 -0500

    Creacion de CompanyDTO

---
+++ PizzeriaMasterpiece.DTO/CompanyDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaMasterpiece.DTO
+{
+    class CompanyDTO
+    {
+        public int CompanyId { get; set; }
+        public String Name { get; set; }
+        public String RUC { get; set; }
+        public String Address { get; set; }
+        public String PhoneNumber { get; set; }
+    }
+}
