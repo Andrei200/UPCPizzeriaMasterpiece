6ee57b0697057221ef46547e30c842f1872ce66d
Alexardela <maxsp_oa@hotmail.com>
2017-05-01 22:09:39 -0500

    update status change in canceled validation

--- PizzeriaMasterpiece.WCFServices/OrderService.svc.cs
+++ PizzeriaMasterpiece.WCFServices/OrderService.svc.cs
@@ -1,12 +1,8 @@
-﻿using System;
+﻿using PizzeriaMasterpiece.DTO;
+using PizzeriaMasterpiece.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-using PizzeriaMasterpiece.DTO;
-using System.Threading.Tasks;
-using PizzeriaMasterpiece.Repository;
 
 namespace PizzeriaMasterpiece.WCFServices
 {
@@ -17,29 +13,24 @@ namespace PizzeriaMasterpiece.WCFServices
         public List<OrderDTO> GetOrdersByClient(int userId)
         {
             var orderRepository = new OrderRepository();
-            return  orderRepository.GetOrdersByClient(userId);
+            return orderRepository.GetOrdersByClient(userId);
         }
 
         public List<OrderWorkerDTO> GetOrdersByCriteria(OrderSearchCriteriaDTO criteria)
         {
             var orderRepository = new OrderRepository();
-            return  orderRepository.GetOrdersByCriteria(criteria);
+            return orderRepository.GetOrdersByCriteria(criteria);
         }
 
         public ResponseDTO UpdateOrderStatus(OrderStatusDTO order)
         {
             try
             {
-
                 var orderRepository = new OrderRepository();
                 var supplyRepository = new SupplyRepository();
                 var productSupplyRepository = new ProductSupplyRepository();
-                var noSupplies = 0;
-            
-                
-               
                 if (order.OrderStatusId == 3)
-                {  
+                {
                     orderRepository.UpdateOrderStatus(order);
                     return new ResponseDTO()
                     {
@@ -47,7 +38,8 @@ namespace PizzeriaMasterpiece.WCFServices
                         Message = "Pedido Cancelado"
                     };
                 }
-                else {
+                else
+                {
                     var currentoOrder = orderRepository.GetOrder(order.OrderId);
                     if (currentoOrder.OrderStatusId != 1)
                     {
@@ -58,21 +50,22 @@ namespace PizzeriaMasterpiece.WCFServices
                         };
                     }
                     var currentSupplies = supplyRepository.GetSupplies();
-                    var usedSupplies =  new List<SupplyDTO>();
+                    var usedSupplies = new List<SupplyDTO>();
                     foreach (var odetail in currentoOrder.OrderDetails)
                     {
-                       var supplies = productSupplyRepository.GetSuppliesByProduct(odetail.ProductId);
+                        var supplies = productSupplyRepository.GetSuppliesByProduct(odetail.ProductId);
                         foreach (var supply in supplies)
                         {
                             var csupply = usedSupplies.FirstOrDefault(p => p.SupplyId == supply.SupplyId);
-                            if (csupply != null) {
-                                csupply.Quantity = csupply.Quantity + supply.Quantity* odetail.Quantity;
+                            if (csupply != null)
+                            {
+                                csupply.Quantity = csupply.Quantity + supply.Quantity * odetail.Quantity;
                             }
                             else
                             {
                                 var newcsupply = new SupplyDTO();
                                 newcsupply.SupplyId = supply.SupplyId;
-                                newcsupply.Quantity = supply.Quantity* odetail.Quantity;
+                                newcsupply.Quantity = supply.Quantity * odetail.Quantity;
                                 usedSupplies.Add(newcsupply);
                             }
                         }
@@ -81,40 +74,31 @@ namespace PizzeriaMasterpiece.WCFServices
                     foreach (var item in usedSupplies)
                     {
                         var csupply = currentSupplies.FirstOrDefault(p => p.SupplyId == item.SupplyId);
-                        if (item.Quantity > csupply.Quantity) { 
-                            noSupplies = 1;
-                            break;
+                        if (item.Quantity > csupply.Quantity)
+                        {
+                            order.OrderStatusId = 3;
+                            orderRepository.UpdateOrderStatus(order);
+                            return new ResponseDTO()
+                            {
+                                Status = 1,
+                                Message = "Pedido Cancelado Automaticamente por falta de Stock"
+                            };
                         }
                     }
 
-                    if (noSupplies == 1) {
-                        order.OrderStatusId = 3;
-                        orderRepository.UpdateOrderStatus(order);
-                        return new ResponseDTO()
-                        {
-                            Status = 1,
-                            Message = "Pedido Cancelado Automaticamente por falta de Stock"
-                        };
-                    }
-                    else
+                    foreach (var item in usedSupplies)
                     {
-                        foreach (var item in usedSupplies)
-                        {
-                            var csupply = currentSupplies.FirstOrDefault(p => p.SupplyId == item.SupplyId);
-                            csupply.Quantity = csupply.Quantity - item.Quantity;
-                            supplyRepository.UpdateSupply(csupply);
-                        }
-                        orderRepository.UpdateOrderStatus(order);
-                        return new ResponseDTO()
-                        {
-                            Status = 1,
-                            Message = "Pedido Aprobado"
-                        };
+                        var csupply = currentSupplies.FirstOrDefault(p => p.SupplyId == item.SupplyId);
+                        csupply.Quantity = csupply.Quantity - item.Quantity;
+                        supplyRepository.UpdateSupply(csupply);
                     }
-
+                    orderRepository.UpdateOrderStatus(order);
+                    return new ResponseDTO()
+                    {
+                        Status = 1,
+                        Message = "Pedido Aprobado"
+                    };
                 }
-
-
             }
             catch (Exception ex)
             {
@@ -123,7 +107,6 @@ namespace PizzeriaMasterpiece.WCFServices
                     Status = 0,
                     Message = "A ocurrido un error"
                 };
-
             }
         }
     }
